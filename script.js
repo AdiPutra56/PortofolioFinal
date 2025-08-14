4579f6cac3da9f64f9d98c63578c8ea83eea5c66
Adi Putra <Madiputra0107@gmail.com>
2025-08-14 09:13:04 +0700

    baru ya

--- script.js
+++ script.js
@@ -31,14 +31,14 @@ $(document).ready(function(){
 
     // typing text animasi
     var typed = new Typed(".typing", {
-        strings: ["Lulusan Baru", "", "IT Support", "Web Development"],
+        strings: ["Lulusan Baru", "", "Administrator", "Product planning and inventory control"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Lulusan Baru", "", "IT Support", "Web Development"],
+        strings: ["Lulusan Baru", "", "Administrator", "Product planning and inventory control"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
